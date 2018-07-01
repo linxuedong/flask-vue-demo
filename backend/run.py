26044b63e9d547323e6c3b0c155d5ff16cf1ed12
Dawn <xuedonglin1994@gmail.com>
2018-07-01 12:20:46 +0800

    feat: random int

--- backend/run.py
+++ backend/run.py
@@ -12,3 +12,11 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
+
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
