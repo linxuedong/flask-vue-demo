4af4e38a2b4b0b787baba12e937584e17133f194
Dawn <xuedonglin1994@gmail.com>
2018-07-03 11:38:54 +0800

    feat: proxy front-end

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 from random import randint
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__,
             static_folder="../frontend/dist/static",
@@ -11,6 +12,8 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
 
 
